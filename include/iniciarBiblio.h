1388e7b199c75ac94eb15289c9ca417d7114398b
VictoriaMaia <victoria.maia@alu.ufc.br>
2020-03-03 11:00:23 -0300

    colocando iniciar que faltou

---
+++ include/iniciarBiblio.h
@@ -0,0 +1,8 @@
+#include <cmath>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
---
+++ include/iniciarFuncs.h
@@ -0,0 +1,10 @@
+// CONV
+Mat camadaConvolucao(Mat image_in);
+
+// POOL
+Mat camadaMaxPool(Mat in);
+
+// DENSE
+void iniar_pesos();
+void camadas_densas(Mat in);
+
